bb0fd30cd60d57f91a50feb621ff418feffb4e75
AnishGupta25 <anishgupta2522@gmail.com>
2021-06-10 21:22:52 +0530

    todo web app completed

--- activity_1/util.js
+++
@@ -1,8 +0,0 @@
-module.exports={
-    Images :['png','jpg','jpeg','gif'],
-    Audio :['mp3'],
-    Documents :['pdf','txt','doc', 'pptx'],
-    Compressed :['zip','rar'],
-    Videos :['mkv','mp4'],
-    Applications : ["exe"]
-}
--- module_1/folder_sorting.js
+++
@@ -1,6 +0,0 @@
-let fs = require("fs");
-
-let data = fs.readFileSync("./pdf.pdf" , "utf-8");
-console.log(data + "");
-
-fs.writeFileSync("pdf.pdf" , "i am pdf file");
